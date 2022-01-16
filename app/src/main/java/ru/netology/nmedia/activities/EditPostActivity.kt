c468fafc5b7ff82c6043fab3bd06d3233601b126
NikitaZar <webbizon@ya.ru>
2022-01-16 11:28:11 +0400

    Task 9 - navigation: edit post activity with intent

---
+++ app/src/main/java/ru/netology/nmedia/activities/EditPostActivity.kt
@@ -0,0 +1,40 @@
+package ru.netology.nmedia.activities
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import ru.netology.nmedia.EditPostResultContract
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.ActivityEditPostBinding
+import ru.netology.nmedia.databinding.ActivityNewPostBinding
+
+class EditPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityEditPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val postText = intent.getStringExtra(Intent.EXTRA_TEXT)
+        binding.editText.setText(postText)
+        binding.editText.requestFocus()
+
+        binding.cancelEditButton.setOnClickListener {
+            setResult(Activity.RESULT_CANCELED, intent)
+            finish()
+        }
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent()
+            if (binding.editText.text.isBlank()) {
+                setResult(Activity.RESULT_CANCELED, intent)
+            } else {
+                val text = binding.editText.text.toString()
+                intent.putExtra(Intent.EXTRA_TEXT, text)
+                setResult(Activity.RESULT_OK, intent)
+            }
+            finish()
+        }
+    }
+}
--- app/src/main/java/ru/netology/nmedia/activities/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activities/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.result.launch
 import androidx.activity.viewModels
 import androidx.constraintlayout.widget.Group
 import androidx.core.view.isVisible
+import ru.netology.nmedia.EditPostResultContract
 import ru.netology.nmedia.NewPostResultContract
 import ru.netology.nmedia.androidUtils.AndroidUtils
 import ru.netology.nmedia.databinding.ActivityMainBinding
@@ -30,6 +31,12 @@ class MainActivity : AppCompatActivity() {
             viewModel.save()
         }
 
+        val editPostLauncher = registerForActivityResult(EditPostResultContract()) { postText ->
+            postText ?: return@registerForActivityResult
+            viewModel.changePostText(postText)
+            viewModel.save()
+        }
+
         val adapter = PostsAdapter(
             object : OnInteractionListener {
                 override fun onLike(post: Post) {
@@ -46,7 +53,6 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onRemove(post: Post) {
                     viewModel.removeById(post.id)
-//                    finishPostEdit(binding.postText, binding.editGroup, viewModel)
                 }
             }
         )
@@ -55,25 +61,11 @@ class MainActivity : AppCompatActivity() {
             newPostLauncher.launch()
         }
 
-//        binding.saveButton.setOnClickListener {
-//            with(binding.postText) {
-//                if (text.isNullOrBlank()) {
-//                    Toast.makeText(
-//                        this@MainActivity,
-//                        R.string.content_cant_be_empty,
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                    return@setOnClickListener
-//                }
-//                viewModel.changePostText(text.toString())
-//                viewModel.save()
-//                finishPostEdit(binding.postText, binding.editGroup, viewModel)
-//            }
-//        }
-//
-//        binding.cancelEditImage.setOnClickListener {
-//            finishPostEdit(binding.postText, binding.editGroup, viewModel)
-//        }
+        viewModel.edited.observe(this) { post ->
+            if (post.id != 0L) {
+                editPostLauncher.launch(post.postText)
+            }
+        }
 
         binding.postsList.adapter = adapter
 
@@ -85,25 +77,5 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
-//        viewModel.edited.observe(this) { post ->
-//            if (post.id == 0L) {
-//                return@observe
-//            }
-//            with(binding.postText) {
-//                requestFocus()
-//                setText(post.postText)
-//            }
-//            binding.editGroup.isVisible = true
-//            binding.editText.text = post.postText
-//        }
     }
-}
-
-fun finishPostEdit(text: EditText, editGroup: Group, viewModel: PostViewModel) {
-    text.setText("")
-    text.clearFocus()
-    AndroidUtils.hideKeyboard(text)
-    viewModel.cancelEdit()
-    editGroup.isVisible = false
 }
